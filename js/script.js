8d491c3679221bef94e372e5e248caa077281a64
Colletta <45748736+colletta-official@users.noreply.github.com>
2018-12-16 06:48:05 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,9 +29,10 @@ function dataLoad()
 {
 	for(var i=0;i<imgList.length;i++)
 	{
+		var title=imgList[i].title.replace( / /gi, '_');
 		var $tac=$("<div class='thumbnail'><img src='img/thumbnail/thumbnail-"+pad(i+1, 2)+".png'></div>");
-		var $popup=$("<div class='popup' onclick='viewPic("+i+")'><div><p class='titlePic'>"+imgList[i].title+"</p><p>"+imgList[i].date+"</p></div></div>");
-		var $wide=$("<div class='wide' id='wp"+i+"'></div>");
+		var $popup=$("<div class='popup' onclick='viewPic("+title+")'><div><p class='titlePic'>"+imgList[i].title+"</p><p>"+imgList[i].date+"</p></div></div>");
+		var $wide=$("<div class='wide' id='"+title+"'></div>");
 		var $widePicArea=$("<div class='widePicArea'></div>");
 		/*
 		for(var j=0; j=imgList[i].src.length; j++)
@@ -52,7 +53,8 @@ function pad(n, width) {
 	n= n + '';
 	return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
 }
+
 function viewPic(i)
 {
-	location.href="#wp"+i;
+	location.href="#"+i;
 }

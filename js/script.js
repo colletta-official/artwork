adabc4bfef95a505528f8b8a9f8fd13e55e65138
Colletta <45748736+colletta-official@users.noreply.github.com>
2018-12-16 05:51:24 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,11 +24,28 @@ function dataLoad()
 {
 	for(var i=0;i<imgList.length;i++)
 	{
-		var $tac=$("<div class='thumbnail'><img src='img/thumbnail/thumbnail-"+pad(i+1, 2)+".png'><div class='popup'><div><p class='titlePic'>"+imgList[i].title+"</p><p>"+imgList[i].date+"</p></div></div></div>");
+		var $tac=$("<div class='thumbnail'><img src='img/thumbnail/thumbnail-"+pad(i+1, 2)+".png'></div>");
+		var $popup=$("<div class='popup' onclick='viewPic("+i+")'><div><p class='titlePic'>"+imgList[i].title+"</p><p>"+imgList[i].date+"</p></div></div>");
+		var $wide=$("<div class='wide' id='wp"+i+"'></div>");
+		var $widePicArea=$("<div class='widePicArea'></div>");
+		for(var j=0; j=imgList[i].src.length; j++)
+		{
+			$widePicArea.append($("<img src='img/"+imgList[i].src[j]+"'>"));
+		}
+		var $wideTextArea=$("<div class='wideTextArea'><div class='realTextArea'>"+imgList[i].description+"</div></div>");
+		$wide.append($widePicArea);
+		$wide.append($wideTextArea);
+		$wide.appendTo($('#wideContainer'));
+		$tac.append($popup);
 		$tac.appendTo($(".list").eq(0));
+		
 	}
 }
 function pad(n, width) {
 	n= n + '';
 	return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
+}
+function viewPic(i)
+{
+	location.href="#wp"+i;
 }

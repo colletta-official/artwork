6881a5b6539ba9b5590be06ab6aa072959e42bb6
Colletta <45748736+colletta-official@users.noreply.github.com>
2018-12-16 02:06:33 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	dataLoad();
-	$(".thumbnail").each(function () {
-		$(this).on("mousewheel DOMMouseScroll", function (e) {
+	$(".scrollBlind")[0].onscroll=function(){
+		$(".thumbnail").each(function (e) {
 			e.preventDefault();
 			var top=$(this).offset().top;
 			var bot=$(".scrollBlind").height();
@@ -9,7 +9,7 @@ $(document).ready(function(){
 			if(top+hei<bot) $(this).addClass('slideInUp');
 			else $(this).removeClass('slideInUp');
 		});
-	});
+	};
 });
 function dataLoad()
 {

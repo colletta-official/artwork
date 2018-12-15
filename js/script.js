d4748865f13192c21202c9deeec98da06f9e0a3e
Colletta <45748736+colletta-official@users.noreply.github.com>
2018-12-16 00:06:10 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function dataLoad()
 {
 	for(var i=0;i<imgList.length;i++)
 	{
-		var $tac=$("<div class='thumbnail'><img src='"+imgList[i].src+"'></div>");
+		var $tac=$("<div class='thumbnail'><img src='img/"+imgList[i].src+"'></div>");
 		$tac.appendTo("#illust");
 	}
 }

f12b95523ab2baced16e497fc4ee4ffc6a100fe4
Colletta <45748736+colletta-official@users.noreply.github.com>
2018-12-16 02:02:21 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
 	dataLoad();
+	$(".thumbnail").each(function () {
+		$(this).on("mousewheel DOMMouseScroll", function (e) {
+			e.preventDefault();
+			var top=$(this).offset().top;
+			var bot=$(".scrollBlind").height();
+			var hei=$(this).height();
+			if(top+hei<bot) $(this).addClass('slideInUp');
+			else $(this).removeClass('slideInUp');
+		}
+	});
 });
 function dataLoad()
 {
 	for(var i=0;i<imgList.length;i++)
 	{
 		var $tac=$("<div class='thumbnail'><img src='img/thumbnail/thumbnail-"+pad(i+1, 2)+".png'></div>");
+		$tac.css('opacity', 0);
 		$tac.appendTo($(".list").eq(0));
 	}
 }
